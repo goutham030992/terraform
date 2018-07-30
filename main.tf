858f1fcb0a0e574f1116677019dee54953c9bea2
Gowtham Sarveswaran <goutham0992@gmail.com>
2018-07-30 15:15:32 -0600

    terraform code to provision ec2 instance

---
+++ main.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+    region = "us-east-1"
+}
+
+
+resource "aws_instance" "masternode" {
+    ami = "ami-b70554c8"
+    instance_type = "t2.micro"
+    
+    tags {
+      Name = "example" 
+}
+
+
+}
+
